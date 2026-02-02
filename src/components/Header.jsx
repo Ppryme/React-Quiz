20ead2732e5fd8d48f3c051d9df5d43948aed8f7
Ppryme <famuyiwapeter2004@gmail.com>
2026-02-02 21:00:05 +0100

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import quizimg from '../assets/quiz-logo.png'
 export default function Header() {
-    return <header>
+    return <header className='text-center '>
         <img src={quizimg} alt="quiz logo" />
-        <h1>React Quiz</h1>
+        <h1 className='text-2xl'>React Quiz</h1>
     </header>
 }
