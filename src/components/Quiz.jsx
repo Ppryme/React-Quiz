e021fc556df98d4e6e2d76a061fb518f92d6a88a
Ppryme <famuyiwapeter2004@gmail.com>
2026-02-02 22:13:51 +0100

    Update Quiz.jsx

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -35,7 +35,7 @@ export default function Quiz() {
 
      
 
-    return <div id="quiz" className=" w-[50px]">
+    return <div id="quiz" className=" max-w-7xl">
                 <Question 
                 key={activeQuestionIndex}
                 index = {activeQuestionIndex}
